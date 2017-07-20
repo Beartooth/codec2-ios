20908b36aed27d73027270067d26b8c5802ea312
Jeff Jones <jeff@beartoothradio.com>
2017-07-19 18:12:16 -0600

    Initial Commit

---
+++ codec2-ios/codec2-ios.h
@@ -0,0 +1,19 @@
+//
+//  codec2-ios.h
+//  codec2-ios
+//
+//  Created by Jefferson Jones on 7/19/17.
+//  Copyright © 2017 Jefferson Jones. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for codec2-ios.
+FOUNDATION_EXPORT double codec2_iosVersionNumber;
+
+//! Project version string for codec2-ios.
+FOUNDATION_EXPORT const unsigned char codec2_iosVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <codec2_ios/PublicHeader.h>
+
+
---
+++ codec2-iosTests/codec2_iosTests.swift
@@ -0,0 +1,36 @@
+//
+//  codec2_iosTests.swift
+//  codec2-iosTests
+//
+//  Created by Jefferson Jones on 7/19/17.
+//  Copyright © 2017 Jefferson Jones. All rights reserved.
+//
+
+import XCTest
+@testable import codec2_ios
+
+class codec2_iosTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
